9357b70c31322eae3c6727608959bc61e6ed79e6
Bushstar <bushsolo@gmail.com>
2019-11-15 14:17:20 +0000

    Python 3 support

--- neoscryptmodule.c
+++ neoscryptmodule.c
@@ -2,30 +2,57 @@
 
 #include "neoscrypt.h"
 
-static PyObject *neoscrypt_getpowhash(PyObject *self, PyObject *args)
+static PyObject *neoscrypt_gost_getpowhash(PyObject *self, PyObject *args)
 {
-    unsigned char *output;
+    char *output;
     PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
     PyStringObject *input;
+#endif
     if (!PyArg_ParseTuple(args, "S", &input))
         return NULL;
     Py_INCREF(input);
     output = PyMem_Malloc(32);
 
-    neoscrypt((unsigned char *)PyString_AsString((PyObject*) input), output);
-
+#if PY_MAJOR_VERSION >= 3
+    neoscrypt((char *)PyBytes_AsString((PyObject*) input), output);
+#else
+    neoscrypt((char *)PyString_AsString((PyObject*) input), output);
+#endif
     Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
     value = Py_BuildValue("s#", output, 32);
+#endif
     PyMem_Free(output);
     return value;
 }
 
-static PyMethodDef NeoScryptMethods[] = {
-    { "getPoWHash", neoscrypt_getpowhash, METH_VARARGS, "Returns proof-of-work hash using NeoScrypt" },
+static PyMethodDef neoscryptMethods[] = {
+    { "getPoWHash", neoscrypt_gost_getpowhash, METH_VARARGS, "Returns the proof of work hash using neoscrypt hash" },
     { NULL, NULL, 0, NULL }
 };
 
-PyMODINIT_FUNC initneoscrypt(void) {
-    (void) Py_InitModule("neoscrypt", NeoScryptMethods);
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef neoscryptModule = {
+    PyModuleDef_HEAD_INIT,
+    "neoscrypt",
+    "...",
+    -1,
+    neoscryptMethods
+};
+
+PyMODINIT_FUNC PyInit_neoscrypt(void) {
+    return PyModule_Create(&neoscryptModule);
 }
+
+#else
+
+PyMODINIT_FUNC initneoscrypt(void) {
+    (void) Py_InitModule("neoscrypt", neoscryptMethods);
+}
+#endif
 
